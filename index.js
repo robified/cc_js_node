8d31e7134a0e80c46e4142bbb6ec1e1fe98d338b
Robin <daorob@gmail.com>
2020-03-29 10:40:13 -0500

    Understanding invisible node functions.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const message = require('./myscript');
+console.log(arguments);
 
-console.log(message);
+console.log('-----');
+console.log(require);
+
+console.log('-----');
+console.log(module);
+
+console.log('-----');
+console.log(__filename);
+
+console.log('-----');
+console.log(__dirname);
